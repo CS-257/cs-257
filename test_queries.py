736bdb3c5d5ea104b5e8057d6ae3dbad6f1154ac
honga2 <honga2@stearns.mathcs.carleton.edu>
2024-02-01 00:05:09 -0500

    Add test queries for planets and species

--- test_queries.py
+++ test_queries.py
@@ -14,30 +14,57 @@ def connect():
 
 
 # Planet test queries
-def query_planets():
+def test_queries():
     try:
         conn = connect()
         cur = conn.cursor()
     except:
         print("There was an issue connecting to the database")
 
-    planet_queries = [
-        # max population in US
+    tests = [
+        # need to use regex to query columns with multiple values
         '''
         SELECT name
         FROM planets
         WHERE orbital_period > 0
         AND climate = 'temperate'
+        AND terrain ~ '\mmountains\M'
         AND population > 0;
+        ''',
+        '''
+        SELECT name
+        FROM planets
+        WHERE rotation_period = 24
+        AND diameter > 12000
+        AND gravity = '1 standard'
+        AND surface_water = 0;
+        ''',
+        '''
+        SELECT name
+        FROM species
+        WHERE designation = 'sentient'
+        AND skin_color ~ '\mbrown\M'
+        AND eye_color ~ '\myellow\M'
+        AND language = 'Huttese';
+        ''',
+        # there's some weird stuff going on with column names here...
+        '''
+        SELECT name
+        FROM species
+        WHERE classification = 'reptile'
+        AND height > 100
+        AND har_color = 'NA'
+        AND lifespan = 0
+        AND homeworld = 'Trandosha';
         ''',
     ]
 
-    if planet_queries is None:
+    if tests is None:
         print("Something went wrong... Please verify that your query is valid.")
         exit()
 
     results = []
-    for _ in planet_queries:
+    for _ in tests:
         cur.execute(_)
         rows = cur.fetchall()
         results.append(rows)
@@ -50,4 +77,4 @@ def query_planets():
 
 
 if __name__ == "__main__":
-    query_planets()
+    test_queries()
