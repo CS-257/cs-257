2298a668377bfbc9c54a18f8675c42880802aff9
joe-bremer <bremerj@carleton.edu>
2024-02-26 13:28:37 -0600

    fix?

---
+++ filters_test/static/js/index.js
@@ -0,0 +1,18 @@
+function setup(){
+    loadCriteria();
+}
+
+function loadCriteria(){
+	var select = document.getElementById('filter_criteria_selector');
+
+    criteriaOptions.forEach(addThisToSelector(select))
+}
+
+function addThisToSelector(selector){
+     var opt = document.createElement('option');
+     opt.value = this;
+     opt.innerHTML = this;
+     selector.appendChild(opt);
+}
+
+//JAVASCRIPT THEN-CHAINING -- FUNCTIONS DONT GET TIME COORDINATED
