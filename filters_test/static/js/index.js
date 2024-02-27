8b90a88ab832c0f9c9bb9a50d1bfa207b8e615f2
joe-bremer <bremerj@carleton.edu>
2024-02-27 14:27:16 -0600

    showing filter selection boxes

--- filters_test/static/js/index.js
+++ filters_test/static/js/index.js
@@ -43,11 +43,11 @@ function cleanTextForDisplay(w){
 
 
 function disableHiddenElements(){
-    document.getElementsByClassName('criteria_filter_options').disabled = true;
+    document.getElementsByClassName('criteria_filter_options').forEach((element) => { element.disabled = true; });
 }
 
 function filterCriteriaSelected(){
-    document.getElementsByClassName('criteria_filter_options_text').disabled = false;
+    document.getElementsByClassName('criteria_filter_options').forEach((element) => { element.disabled = false; });
 }
 
 //JAVASCRIPT THEN-CHAINING -- FUNCTIONS DONT GET TIME COORDINATED
