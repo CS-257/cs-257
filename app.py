cbdda6d39af44c17f7d923adbd6b7c7ef79890d6
joe-bremer <bremerj@carleton.edu>
2024-03-10 15:06:03 -0500

    dynamically adding elements

--- app.py
+++ app.py
@@ -28,114 +28,32 @@ def home():
 
 @app.route('/search/<category>')
 def search_category(category):
-    return render_template('category_search_page.html')
+    return render_template('category_search_page.html', category=category)
+
 
 # Define a route to handle the request for character information
-@app.route('/characters-info', methods=['POST'])
-def characters_info():
+@app.route('/fetch-category-element-names', methods=['POST'])
+def fetch_category_element_names():
     try:
-        # Extract species name from request
         request_data = request.get_json()
-        characters_name = request_data.get('characters')
+        category = request_data.get('fetch_from_category')
 
         # Query the database for species information
         conn = connect_to_db()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM characters WHERE name = %s", (characters_name,))
-        characters_info = cursor.fetchone()
+        cursor.execute("SELECT name FROM %s", (category,))
+        info = cursor.fetchall()
         cursor.close()
         conn.close()
 
-        if characters_info:
-            # If character information is found, return it as JSON
-            return jsonify(characters_info)
+        if info:
+            return jsonify(info)
+
         else:
-            # If species not found, return appropriate response
-            return jsonify({'error': 'Character not found'}), 404
+            # Error if category not found
+            return jsonify({'error': 'Category not found'}), 404
+
     except Exception as e:
-        # Handle any exceptions that occur during processing
-        return jsonify({'error': str(e)}), 500
-
-# Define a route to handle the request for species information
-@app.route('/species-info', methods=['POST'])
-def species_info():
-    try:
-
-        # Extract species name from request
-        request_data = request.get_json()
-        species_name = request_data.get('species')
-
-        # Query the database for species information
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM species WHERE name = %s", (species_name,))
-        species_info = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        if species_info:
-            # If species information is found, return it as JSON
-            return jsonify(species_info)
-        else:
-            # If species not found, return appropriate response
-            return jsonify({'error': 'Species not found'}), 404
-    except Exception as e:
-        # Handle any exceptions that occur during processing
-        return jsonify({'error': str(e)}), 500
-
-# Define a route to handle the request for starships information
-@app.route('/starships-info', methods=['POST'])
-def starships_info():
-    try:
-
-        # Extract starships name from request
-        request_data = request.get_json()
-        starships_name = request_data.get('starships')
-
-        # Query the database for starships information
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM starships WHERE name = %s", (starships_name,))
-        starships_info = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        if starships_info:
-            # If starships information is found, return it as JSON
-            return jsonify(starships_info)
-        else:
-            # If starships not found, return appropriate response
-            return jsonify({'error': 'Starships not found'}), 404
-    except Exception as e:
-        # Handle any exceptions that occur during processing
-        return jsonify({'error': str(e)}), 500
-
-
-# Define a route to handle the request for vehicles information
-@app.route('/vehicles-info', methods=['POST'])
-def vehicles_info():
-    try:
-
-        # Extract vehicles name from request
-        request_data = request.get_json()
-        vehicles_name = request_data.get('vehicles')
-
-        # Query the database for starships information
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM vehicles WHERE name = %s", (vehicles_name,))
-        vehicles_info = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        if vehicles_info:
-            # If vehicles information is found, return it as JSON
-            return jsonify(vehicles_info)
-        else:
-            # If vehicles not found, return appropriate response
-            return jsonify({'error': 'Vehicles not found'}), 404
-    except Exception as e:
-        # Handle any exceptions that occur during processing
         return jsonify({'error': str(e)}), 500
 
 
---
+++ static/js/category_search_page.js
@@ -0,0 +1,93 @@
+//VARIABLES CREATED
+//var category
+
+
+// Wait until DOM content loaded
+document.addEventListener('DOMContentLoaded', function() {
+    buildCategoryElementList(category,'search-list'))// Generates list of category elements
+    
+    // Add the 'click' event listener to handle any 'click' action from user
+    charactersList.addEventListener('click', function(event) {
+
+        // Check if the clicked element is a list type
+        if (event.target.tagName === 'LI') {
+
+            var charactersName = event.target.dataset.characters;
+	        // Call function that displays character information on click
+            toggleCharactersInformation(charactersName, event.target);
+
+        }
+
+    });
+    
+
+    /*
+    // Get the search input item from the HTML doc 
+    var searchInput = document.getElementById('search');
+
+    // Add event listener for the search action 
+    searchInput.addEventListener('input', function() {
+        var searchValue = searchInput.value.toLowerCase();
+        // Get all items that are of type list from the page
+        var charactersItems = charactersList.querySelectorAll('li');
+
+        charactersItems.forEach(function(item) {
+            var charactersName = item.dataset.characters.toLowerCase();
+            // Check if the character name includes the search value
+            if (charactersName.includes(searchValue)) {
+                // If the character name matches the search value, show the list item
+                item.style.display = 'block';
+            } else {
+                // If the character name does not match the search value, hide the list item
+                item.style.display = 'none';
+                // Check if there is an info box associated with the list item
+                var infoBox = item.nextElementSibling;
+                if (infoBox && infoBox.classList.contains('info-box')) {
+                    // If an info box exists, remove it
+                    infoBox.remove();
+                }
+            }
+        });
+    });*/
+});
+
+
+
+//fetches names of all elements in a category (e.g. all species, all characters, etc.)
+//then adds these names to the display as list elements
+function buildCategoryElementList(fetchingFromCategory,htmlListID) {
+
+    fetch('/characters-info', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            fetch_from_category : fetchingFromCategory
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Call function to display information
+        buildCategoryElementListHTML(data,htmlListID)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+}
+
+//adds list elements to the page based on what's returned by the sql query
+function buildCategoryElementListHTML(elements,htmlListID){
+    var elementsList = document.getElementById('search-list'); // Gets list element
+
+    for(let i = 0; i < elements.length; i++){
+        let element = elements[i];
+
+        let entry = document.createElement('li');
+        entry.value = element;
+        entry.appendChild(document.createTextNode(element));
+        
+        elementsList.appendChild(entry);
+    }
+}
